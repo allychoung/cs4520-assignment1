7b22577e9362c7bcd06a430b56df78ce6021158f
allychoung <allychoung@gmail.com>
2024-02-13 15:57:39 -0500

    add main activity, login screen layout

--- app/src/main/java/com/cs4520/assignment1/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -1,2 +1,12 @@
 package com.cs4520.assignment1
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
